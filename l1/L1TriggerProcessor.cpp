b5fc31f2cf428b1a4561e32368e2d02959345050
Jonas Kunze <kunze.jonas@gmail.com>
2014-10-10 17:21:30 +0200

    Moved L0TP readout to L1Builder

--- l1/L1TriggerProcessor.cpp
+++ l1/L1TriggerProcessor.cpp
@@ -12,25 +12,13 @@
 #include <l0/MEPFragment.h>
 #include <l0/Subevent.h>
 
-#include "../common/headers/L0TPHeader.h"
-
 namespace na62 {
 
-uint16_t L1TriggerProcessor::compute(Event* event) {
+uint8_t L1TriggerProcessor::compute(Event* event) {
 	using namespace l0;
-
-	/*
-	 * Store
-	 */
-	l0::MEPFragment* L0TPEvent = event->getL0TPSubevent()->getFragment(0);
-	L0TpHeader* L0TPData = (L0TpHeader*) L0TPEvent->getPayload();
-	event->setFinetime(L0TPData->refFineTime);
 
 	event->setProcessingID(0); // 0 indicates raw data as collected from the detector
 
-	uint8_t l0TriggerTypeWord = L0TPData->l0TriggerType;
-	uint8_t l1TriggerTypeWord = 1; // Accept event
-
-	return (l0TriggerTypeWord | l1TriggerTypeWord << 8);
+	return 1;
 }
 } /* namespace na62 */
