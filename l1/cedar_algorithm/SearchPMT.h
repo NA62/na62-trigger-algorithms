ce2c85c7e7dfaa1a26f931edecb798bd37554242
Angela Romano <angela.romano@cern.ch>
2014-12-11 11:51:27 +0100

    On branch feature/ktag Your branch is up-to-date with 'origin/feature/ktag'.
    Changes to be committed:
    	modified:   ../.cproject
    	modified:   L1TriggerProcessor.cpp
    	modified:   L1TriggerProcessor.h
    	new file:   cedar_algorithm/SearchPMT.cpp
    	new file:   cedar_algorithm/SearchPMT.h
    	new file:   cedar_algorithm/tdcb_buffer.cpp
    	new file:   cedar_algorithm/tdcb_buffer.h
    	new file:   data_decoder/CedarData.cpp
    	new file:   data_decoder/CedarData.h

---
+++ l1/cedar_algorithm/SearchPMT.h
@@ -0,0 +1,29 @@
+/*
+ * SearchPMT.h
+ *
+ *  Created on: Nov 24, 2014
+ *      Author: angela romano
+ *      Email: axr@hep.ph.bham.ac.uk
+ */
+
+#ifndef SEARCHPMT_H_
+#define SEARCHPMT_H_
+
+
+int SearchPMT(int tel, int pp, int tdc, int ch);
+/*
+namespace na62
+    {
+
+    class SearchPMT
+	{
+	public:
+	  //SearchPMT();
+	  int search_pmt(int tel, int pp, int tdc, int ch);
+	  
+	private:
+	};
+
+    }
+*/
+#endif /* SEARCHPMT_H_ */
