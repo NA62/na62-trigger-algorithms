baf16df9ef9c59671908f7025755be58e5ac5ae9
Angela Romano <angela.romano@cern.ch>
2015-06-16 18:34:40 +0200

    - Added L2Fragment header file with L2Block

--- l2/L2Fragment.h
+++ l2/L2Fragment.h
@@ -1,23 +1,22 @@
 /*
- * L1Fragment.h
+ * L2Fragment.h
  *
  *  Created on: Jun 16, 2015
  *      Author: Angela Romano (angela.romano@cern.ch)
  */
 
 #pragma once
-#ifndef L1Fragment_H_
-#define L1Fragment_H_
+#ifndef L2Fragment_H_
+#define L2Fragment_H_
 
 namespace na62 {
 
 
-struct L1_BLOCK {
+struct L2_BLOCK {
 	uint32_t triggerword;
 };
 
 }
 
 
-
-#endif    /*  L1Fragment_H */
+#endif    /*  L2Fragment_H */
