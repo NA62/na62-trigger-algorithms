294aa1048e8d7c9d2c04ba5305e791e3a251fbf6
Jonas Kunze <kunzej.jonas@gmail.com>
2015-06-16 16:00:49 +0200

    - Added L1Fragment header file with L1Block

---
+++ l1/L1Fragment.h
@@ -0,0 +1,23 @@
+/*
+ * L1Fragment.h
+ *
+ *  Created on: Jun 16, 2015
+ *      Author: Jonas Kunze (kunze.jonas@gmail.com)
+ */
+
+#pragma once
+#ifndef L1Fragment_H_
+#define L1Fragment_H_
+
+namespace na62 {
+
+
+struct L1_BLOCK {
+	uint32_t triggerword;
+};
+
+}
+
+
+
+#endif    /*  L1Fragment_H */
