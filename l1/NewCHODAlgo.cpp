13b2d53ba7a9058eef86d8be8844191685199c39
LorenzaIacobuzio <lorenza.iacobuzio@gmail.com>
2016-05-20 10:59:07 +0100

    Modified NewCHODAlgo.

--- l1/NewCHODAlgo.cpp
+++ l1/NewCHODAlgo.cpp
@@ -1,8 +1,8 @@
 /*
  * NewCHODAlgo.cpp
  *
- *  Created on: 13 May 2016
- *      Author: lorenza
+ *  Created on: 16 May 2016
+ *      Author: LorenzaIacobuzio
  */
 
 #include "NewCHODAlgo.h"
@@ -18,16 +18,29 @@
 #include <options/Logging.h>
 
 #include "../common/decoding/TrbFragmentDecoder.h"
+#include "newchod_algorithm/ParsConfFile.h"
 
 namespace na62 {
 
+uint NewCHODAlgo::algoID; //0 for CHOD, 1 for RICH, 2 for KTAG, 3 for LAV, 4 for MUV3, 5 for Straw
+uint NewCHODAlgo::algoLogic;
+uint NewCHODAlgo::algoRefTimeSourceID;
+double NewCHODAlgo::algoOnlineTimeWindow;
+
+bool NewCHODAlgo::algoProcessed = 0;
+bool NewCHODAlgo::emptyPacket = 0;
+bool NewCHODAlgo::badData = 0;
+bool NewCHODAlgo::isNewCHODRefTime = 0;
+
+double NewCHODAlgo::averageHitTime = 0.;
 NewCHODParsConfFile* NewCHODAlgo::infoNewCHOD_ = NewCHODParsConfFile::GetInstance();
-int * NewCHODAlgo::pmtGeo = infoNewCHOD_->getGeoPMTMap();
-//uint NewCHODAlgo::nHits;
-//uint NewCHODAlgo::nMaxPMTs;
-uint NewCHODAlgo::pmtID1;
-uint NewCHODAlgo::pmtID2;
-int NewCHODAlgo::roChID;
+int * NewCHODAlgo::PMTGeo = infoNewCHOD_->getGeoPMTMap();
+uint NewCHODAlgo::nHits;
+uint NewCHODAlgo::nMaxPMTs;
+int NewCHODAlgo::PMTID1;
+int NewCHODAlgo::PMTID2;
+//int NewCHODAlgo::quadrantID;
+//int NewCHODAlgo::planeID;
 
 NewCHODAlgo::NewCHODAlgo() {
 }
@@ -36,62 +49,120 @@ NewCHODAlgo::~NewCHODAlgo() {
 // TODO Auto-generated destructor stub
 }
 
-uint_fast8_t NewCHODAlgo::processNewCHODTrigger(DecoderHandler& decoder) {
+/*void NewCHODAlgo::initialize(l1NewCHOD &l1NewCHODStruct) {
+
+	algoID = l1NewCHODStruct.configParams.l1TrigMaskID;
+	algoLogic = l1NewCHODStruct.configParams.l1TrigLogic;
+	algoRefTimeSourceID = l1NewCHODStruct.configParams.l1TrigRefTimeSourceID; //0 for L0TP, 1 for CHOD, 2 for RICH
+	algoOnlineTimeWindow = l1NewCHODStruct.configParams.l1TrigOnlineTimeWindow;
+
+}*/
+
+uint_fast8_t NewCHODAlgo::processNewCHODTrigger(DecoderHandler& decoder,
+		L1InfoToStorage* l1Info) {
+
+//	LOG_INFO("Initial Time " << time[0].tv_sec << " " << time[0].tv_usec);
 
 	using namespace l0;
 
-	//nMaxPMTs = 0;
-	//nHits = 0;
+	nMaxPMTs = 6;
+	nHits = 0;
 
-	LOG_INFO("Event number = " << decoder.getDecodedEvent()->getEventNumber());
-	LOG_INFO("Timestamp = " << std::hex << decoder.getDecodedEvent()->getTimestamp() << std::dec);
+//	LOG_INFO("Event number = " << decoder.getDecodedEvent()->getEventNumber());
+//	LOG_INFO("NewCHODAlgo: event timestamp = " << std::hex << decoder.getDecodedEvent()->getTimestamp() << std::dec);
+//	LOG_INFO("Average Hit Time (initial value) " << averageHitTime);
 
 	TrbFragmentDecoder& newchodPacket =
 			(TrbFragmentDecoder&) decoder.getDecodedNewCHODFragment(0);
+	if (!newchodPacket.isReady() || newchodPacket.isBadFragment()) {
+
+		LOG_ERROR ("NewCHODAlgo: This looks like a Bad Packet!!!! ");
+		badData = 1;
+		return 0;
+	}
+//	LOG_INFO("First time check (inside iterator) " << time[1].tv_sec << " " << time[1].tv_usec);
 	/**
 	 * Get Arrays with hit Info
 	 */
-
 	const uint64_t* const edge_times = newchodPacket.getTimes();
 	const uint_fast8_t* const edge_chIDs = newchodPacket.getChIDs();
 	const bool* const edge_IDs = newchodPacket.getIsLeadings();
 	const uint_fast8_t* const edge_tdcIDs = newchodPacket.getTdcIDs();
-	double finetime, edgetime;
+	double finetime, edgetime1, edgetime2;
 
 	uint numberOfEdgesOfCurrentBoard = newchodPacket.getNumberOfEdgesStored();
-//	LOG_INFO("newchod: Tel62 ID " << newchodPacket.getFragmentNumber() << " - Number of Edges found " << numberOfEdgesOfCurrentBoard);
+	if (!numberOfEdgesOfCurrentBoard)
+		emptyPacket = 1;
 
 	for (uint iEdge = 0; iEdge != numberOfEdgesOfCurrentBoard; iEdge++) {
-
-		edgetime = (edge_times[iEdge]
-				- decoder.getDecodedEvent()->getTimestamp() * 256.)
-				* 0.097464731802;
-
-//		LOG_INFO("finetime (decoder) " << (uint)decoder.getDecodedEvent()->getFinetime());
-//		LOG_INFO("edge_time " << std::hex << edge_times[iEdge] << std::dec);
-		LOG_INFO("finetime (in ns) " << finetime);
-		LOG_INFO("edgetime (in ns) " << edgetime);
-//		LOG_INFO("Without offset " << fabs(edgetime - finetime));
-
-		/**
-		 * Process leading edges only
-		 *
-		 */
-		if (edge_IDs[iEdge]) {
-			roChID = (edge_tdcIDs[iEdge] * 32) + edge_chIDs[iEdge];
-			if (pmtGeo[roChID] > 0) pmtID1 = pmtGeo[roChID] - 50;
-			else pmtID1 = pmtGeo[roChID];
-			for (uint jEdge = 0; jEdge != numberOfEdgesOfCurrentBoard; jEdge++) {
-				if (edge_IDs[jEdge] && jEdge != iEdge) {
-					roChID = (edge_tdcIDs[jEdge] * 32) + edge_chIDs[jEdge];
-					if (pmtGeo[roChID] > 151) pmtID2 = pmtGeo[roChID] - 50;
-					else pmtID2 = pmtGeo[roChID];
+		const int roChID = (edge_tdcIDs[iEdge] * 32) + edge_chIDs[iEdge];
+		PMTID1 = PMTGeo[roChID];
+		LOG_INFO("Readout Channel ID " << roChID);
+		LOG_INFO("Geom PMT ID " << PMTID1);
+		for (uint jEdge = 0; jEdge != numberOfEdgesOfCurrentBoard; jEdge++) {
+			const int roChID = (edge_tdcIDs[jEdge] * 32) + edge_chIDs[jEdge];
+			PMTID2 = PMTGeo[roChID];
+			LOG_INFO("Readout Channel ID " << roChID);
+			LOG_INFO("Geom PMT ID " << PMTID2);
+			if (fabs(PMTID1-PMTID2) == 50) {
+				finetime = decoder.getDecodedEvent()->getFinetime()
+						* 0.097464731802;
+				edgetime1 = (edge_times[iEdge]
+						- decoder.getDecodedEvent()->getTimestamp() * 256.)
+						* 0.097464731802;
+				edgetime2 = (edge_times[jEdge]
+						- decoder.getDecodedEvent()->getTimestamp() * 256.)
+						* 0.097464731802;
+				if(fabs(edgetime1-edgetime2) < 20) {
+					averageHitTime += (edgetime1 + edgetime2)/2;
+					nHits++;
 				}
 			}
 		}
 	}
+
+	if (nHits)
+		averageHitTime = averageHitTime/nHits;
+	else
+		averageHitTime = -1.0e+28;
+
+	algoProcessed = 1;
+
+	//l1Info->setNewCHODAverageTime(averageHitTime);
+	//l1Info->setL1NewCHODProcessed();
+
+//	LOG_INFO("NewCHODAlgo=============== number of Hits " << nHits_V + nHits_H);
+//	LOG_INFO("NewCHODAlgo=============== average HitTime " << averageHitTime);
+//	LOG_INFO("NewCHODAlgo=============== L1NewCHODProcessed Flag " << (uint)l1Info->isL1NewCHODProcessed());
+
+	averageHitTime = 0;
+//	LOG_INFO("NewCHODAlgo=============== reset average HitTime " << averageHitTime);
+
+/*	if (algoLogic)
+		return ((nHits > 0) && ((nHits) < nMaxPMTs));
+	else
+		return (nHits >= nMaxPMTs);*/
+	if (nHits == 1)
+		return averageHitTime;
+	else
+		return 0;
 }
+
+bool NewCHODAlgo::isAlgoProcessed() {
+	return algoProcessed;
+}
+
+void NewCHODAlgo::resetAlgoProcessed() {
+	algoProcessed = 0;
+}
+
+bool NewCHODAlgo::isEmptyPacket() {
+	return emptyPacket;
+}
+
+bool NewCHODAlgo::isBadData() {
+	return badData;
+}
+
 }
 /* namespace na62 */
-
-
--- l1/NewCHODAlgo.h
+++ l1/NewCHODAlgo.h
@@ -1,21 +1,26 @@
 /*
  * NewCHODAlgo.h
  *
- *  Created on: 13 May 2016
+ *  Created on: 16 May 2016
  *      Author: lorenza
  */
 
+#pragma once
 #ifndef NEWCHODALGO_H_
 #define NEWCHODALGO_H_
+
 
 #include <sys/types.h>
 #include <cstdint>
 
 #include "../common/decoding/DecoderHandler.h"
 #include "newchod_algorithm/ParsConfFile.h"
+#include "L1InfoToStorage.h"
+#include "../struct/HLTConfParams.h"
+
+#define maxNhits 500
 
 namespace na62 {
-
 
 class NewCHODAlgo {
 public:
@@ -29,20 +34,41 @@ public:
 
 	NewCHODAlgo();
 	~NewCHODAlgo();
-	static uint_fast8_t processNewCHODTrigger(DecoderHandler& decoder);
+
+	static uint_fast8_t processNewCHODTrigger(DecoderHandler& decoder, L1InfoToStorage* l1Info);
+//	static void initialize(l1NewCHOD &l1NewChodStruct);
+
+	static bool isAlgoProcessed();
+	static void resetAlgoProcessed();
+	static bool isEmptyPacket();
+	static bool isBadData();
 
 private:
 
 	static NewCHODParsConfFile* infoNewCHOD_;
-	static int* pmtGeo;
-	//static uint nHits;
-	//static uint nMaxPMTs;
-	static uint pmtID1;
-	static uint pmtID2;
-	static int roChID;
+	static uint algoID; //0 for CHOD, 1 for RICH, 2 for KTAG, 3 for LAV, 4 for MUV3, 5 for Straw
+	static uint algoLogic;
+	static uint algoRefTimeSourceID;
+	static double algoOnlineTimeWindow;
+
+	static bool algoProcessed;
+	static bool emptyPacket;
+	static bool badData;
+	static bool isNewCHODRefTime;
+
+	static int* PMTGeo;
+	static uint nHits;
+	static uint nMaxPMTs;
+	static int PMTID1;
+	static int PMTID2;
+//	static int quadrantID;
+//	static int planeID;
+	static double averageHitTime;
 
 };
 
 } /* namespace na62 */
+
+
 
 #endif /* NEWCHODALGO_H_ */
